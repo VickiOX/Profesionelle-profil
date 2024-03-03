1ed8ee6dbd9e71a9691dbc743873277b02366ab6
VickiOx <viox0001@stud.kea.dk>
2024-03-03 17:42:17 +0100

    slet daisyui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,5 @@ module.exports = {
         require('@tailwindcss/forms'),
         require('@tailwindcss/aspect-ratio'),
         require('@tailwindcss/container-queries'),
-        require("daisyui"),
     ],
   };
